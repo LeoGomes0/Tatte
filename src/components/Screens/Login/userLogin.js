bd3948a255a536e8a6be994d59b37d3151d50862
LeoGomes0 <leonardobrgomes@outlook.com>
2024-04-01 20:17:42 -0300

    Criando projeto Tatte

---
+++ src/components/Screens/Login/userLogin.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+
+const Login = () => {
+
+
+  return (
+
+    <View>
+
+      <View></View>
+
+      <View></View>
+
+      <View></View>
+
+    </View>
+
+  );
+
+
+}
+
+export default Login;
